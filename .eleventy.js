44b5ea2aeb8fb66651c0bca361720220141dc77e
Ivo Valchev <ivo.v.valchev@gmail.com>
2021-05-24 16:18:10 +0200

    Add side section with meta data

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,24 @@
 const Nunjucks = require("nunjucks");
-const truncateHtml = require('truncate-html')
+const truncateHtml = require('truncate-html');
+const luxon = require('luxon');
+const yaml = require('js-yaml');
 
 module.exports = function(eleventyConfig) {
     const nunjucksEnvironment = new Nunjucks.Environment(
         new Nunjucks.FileSystemLoader("_includes")
     );
 
-    eleventyConfig.addNunjucksFilter("date", function(iso) {
-        return (new Date(iso)).toDateString();
+    eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+
+    eleventyConfig.addNunjucksFilter("date", function(dateString, format = 'cccc L LLLL yyyy', relative = false) {
+        let date;
+        if (dateString === 'now') {
+            date = luxon.DateTime.now();
+        } else {
+            date = luxon.DateTime.fromISO(dateString);
+        }
+
+       return relative ? date.toRelativeCalendar() : date.toFormat(format);
     });
 
     eleventyConfig.addNunjucksFilter('truncate_html', function(html, length=20) {
